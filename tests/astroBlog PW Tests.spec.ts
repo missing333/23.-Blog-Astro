7215305e9471078b4653a3136e303edf64720412
Marc <mmissing3@gmail.com>
2023-10-14 12:03:51 -0700

    added mobile testing

--- tests/astroBlog PW Tests.spec.ts
+++ tests/astroBlog PW Tests.spec.ts
@@ -20,22 +20,31 @@ test.describe('New Greeting', () => {
 });
 
 test('has title', async ({ page }) => {
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Home Page/);
 });
 
+async function findAndClickBurger(page) {
+  const burger = page.locator('.hamburger');
+  if(await burger.isVisible()){
+    await burger.click();
+  }  
+}
+
 test('about link takes me to about page',async ({page}) => {
+  findAndClickBurger(page);  
   await page.getByRole('link', { name: 'About' }).click();
   await expect(page).toHaveTitle(/About Me/);
 })
 
 test('blog link takes me to blog page',async ({page}) => {
+  findAndClickBurger(page);
   await page.getByRole('link', { name: 'Blog' }).click();
   await expect(page).toHaveTitle(/Blog/);
 })
 
 test('tags link takes me to tags page',async ({page}) => {
+  findAndClickBurger(page);
   await page.getByRole('link', { name: 'tags' }).click();
   await expect(page).toHaveTitle(/Tags/);
 })
