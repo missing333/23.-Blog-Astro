c38c8c8445adbc4de96714c427460c2f95a10fea
Marc <mmissing3@gmail.com>
2023-10-14 12:21:08 -0700

    default greeting no longer in random bucket

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,7 +3,7 @@ import { useState } from 'preact/hooks';
 export default function Greeting({messages}) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-  const [greeting, setGreeting] = useState(messages[0]);
+  const [greeting, setGreeting] = useState("Hi");
 
   return (
     <div>
